98174519dcfae5474a911e873bbd5db82c2a2768
Darek <wojak1011@gmail.com>
2018-08-18 23:41:15 +0200

    23

--- commands/shop.js
+++ commands/shop.js
@@ -11,7 +11,7 @@ module.exports.run = async (bot, message, args) => {
   .addField("Rank Noob:", "1$")
   .addField("Rank Vip:", "100000$")
   .addField("Rank Gracz:", "10$")
-  .addField("Rank Dobry:", "Gracz 100$")
+  .addField("Rank Dobry Gracz:", "100$")
   .addField("Rank Youtuber:", "6000$")
   .addField("Rank Moderator:", "1000000$");
 
