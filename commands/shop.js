469bc6c789c3953c6b27eff12c3b1ddb9a944654
Darek <wojak1011@gmail.com>
2018-08-18 22:51:33 +0200

    2

--- commands/shop.js
+++ commands/shop.js
@@ -9,12 +9,12 @@ module.exports.run = async (bot, message, args) => {
   .setColor("#f4e802")
   .setTitle("Server Shop")
   .addField("Rank Admin", "100000000$")
-  .addBlankField("Rank Noob", "1$")
-  .addBlankField("Rank Vip", "100000$")
-  .addBlankField("Rank Gracz", "10$")
-  .addBlankField("Rank Dobry Gracz", "100$")
-  .addBlankField("Rank Youtuber", "6000$")
-  .addBlankField("Rank Moderator", "1000000$");
+  .addBlankField("Rank Noob", "1$", true)
+  .addBlankField("Rank Vip", "100000$", true)
+  .addBlankField("Rank Gracz", "10$", true)
+  .addBlankField("Rank Dobry Gracz", "100$", true)
+  .addBlankField("Rank Youtuber", "6000$", true)
+  .addBlankField("Rank Moderator", "1000000$", true);
 
   message.channel.send(coinEmbed).then(msg => {msg.delete(20000)});
 }
