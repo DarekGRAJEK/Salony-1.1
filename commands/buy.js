75cbfda7abade94addc3ca1b04656d4ea5ff22a2
DarekGRAJEK <41697254+DarekGRAJEK@users.noreply.github.com>
2018-08-19 13:09:51 +0200

    16859

--- commands/buy.js
+++ commands/buy.js
@@ -6,12 +6,12 @@ module.exports.run = async (bot, message, args) => {
     await message.delete();
     let mBuy = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     let mto = args.join(" ").slice(22);
-    let gAdmin = message.guild.roles.find(`name`, "!!!Admin!!!");
+    let gAdmin = message.guild.roles.find(`name`, "! ! ! Admin ! ! !");
     let gVip = message.guild.roles.find(`name`, "VIP");
-    let gGracz = message.guild.roles.find(`name`, "!!!Gracz!!!");
-    let gDobryGracz = message.guild.roles.find(`name`, "!!!dobry gracz!!!");
+    let gGracz = message.guild.roles.find(`name`, "! ! ! Gracz ! ! !");
+    let gDobryGracz = message.guild.roles.find(`name`, "! ! !dobry gracz ! ! !");
     let gNoob = message.guild.roles.find(`name`, "Noob");
-    let gmoderator = message.guild.roles.find(`name`, "!!!Moderator!!!");
+    let gmoderator = message.guild.roles.find(`name`, "! ! ! Moderator! ! !");
     let gYotuber = message.guild.roles.find(`name`, "Youtuber");
     let uCoins = coins[message.author.id].coins;
     let MAdmin = 100000000;
