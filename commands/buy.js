6871688acbfd97431e9ecee1985be7db0eddd184
DarekGRAJEK <41697254+DarekGRAJEK@users.noreply.github.com>
2018-08-19 11:57:52 +0200

    2546

--- commands/buy.js
+++ commands/buy.js
@@ -35,52 +35,60 @@ module.exports.run = async (bot, message, args) => {
     if (mTo == "Admin" && uCoins == MAdmin)
     { 
         await(mBuy.addRole(gAdmin.id));
+        if (uCoins == MAdmin) {
         coins[message.author.id] = {
             coins: coins[message.author.id].coins - MAdmin
-        };
+           };
+        } 
       
     }else if (mto == "Moderator" && uCoins == MMod)
     {
         await(mBuy.addRole(gmoderator.id));
+        if ( uCoins == MMod) {
         coins[message.author.id] = {
             coins: coins[message.author.id].coins - MMod
         };
-        
+       } 
     }else if (mto == "Dobry Gracz" && uCoins == MDbGracz)
     {
         await(mBuy.addRole(gDobryGracz.id));
+        if (uCoins == MDbGracz) {
         coins[message.author.id] = {
             coins: coins[message.author.id].coins - MDbGracz
         };
-       
+      } 
     }else if (mto == "Noob" && uCoins == MNoob)
     {
         await(mBuy.addRole(gNoob.id));
+        if (uCoins == MNoob) {
         coins[message.author.id] = {
             coins: coins[message.author.id].coins - MNoob
         };
-        
+       } 
     }else if (mto == "Gracz" && uCoins == MGracz)
     {
         await(mBuy.addRole(gGracz.id));
+        if (uCoins == MGracz) {
         coins[message.author.id] = {
             coins: coins[message.author.id].coins - MGracz
         };
-        
+       } 
     }else if (mto == "Vip" && uCoins == MVip)
     {
         await(mBuy.addRole(gVip.id));
+        if (uCoins == MVip) {
         coins[message.author.id] = {
             coins: coins[message.author.id].coins - MVip
         };
-        
+       } 
     }else if (mto == "Youtuber" && uCoins == MYt)
     {
         await(mBuy.addRole(gYotuber.id));
+        if (uCoins == MYt) {
         coins[message.author.id] = {
             coins: coins[message.author.id].coins - MYt
         };
-        
+       } 
     } else {
         message.replay("You do not have enough money");
     }
