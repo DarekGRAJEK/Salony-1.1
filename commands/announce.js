61367cad67b251064f34569c86c586b206a27006
Darek <wojak1011@gmail.com>
2018-08-31 11:39:15 +0200

    2855

---
+++ commands/announce.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const errors = require("../utility/error.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  message.delete();
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+  let annmessage = args.join(" ");
+  
+      let reportEmbed = new Discord.RichEmbed()
+    .setTitle(`Ogłoszenie - ${message.author}`)
+    .setColor("#00FF00")
+    .setTimestamp()
+    .setDescription(annmessage);
+
+    let reportschannel = message.guild.channels.find(`name`, `ogłoszenia`);
+    if(!reportschannel) return message.channel.send("Couldn't find announcement channel");
+
+    reportschannel.send(reportEmbed);
+}
+
+module.exports.help = {
+  name: "olg"
+}
