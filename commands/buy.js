158e1ff55be7dab4b715f0bbbf9444870039f668
Darek <wojak1011@gmail.com>
2018-09-02 00:50:09 +0200

    Odj

--- commands/buy.js
+++ commands/buy.js
@@ -5,6 +5,7 @@ let fs = require("fs");
 module.exports.run = async (bot, message, args) => {
     await message.delete();
     let mto = args.join(" ").slice(22);
+    let mbuy = message.author;
     let gAdmin = message.guild.roles.find(`name`, "! ! ! Admin ! ! !");
     let gVip = message.guild.roles.find(`name`, "VIP");
     let gGracz = message.guild.roles.find(`name`, "! ! ! Gracz ! ! !");
