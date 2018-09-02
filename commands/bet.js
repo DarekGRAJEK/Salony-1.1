30e18a217e26f089c3e2d1a2b8af9c3f8bafc4da
Darek <wojak1011@gmail.com>
2018-09-02 14:15:28 +0200

    ppl

---
+++ commands/bet.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+let bets = require("../coins.json");
+
+module.exports.run = async (bot, message, args) => {
+  //!bet
+  await message.delete();
+  if(!bets[message.author.id]){
+    bets[message.author.id] = {
+      bet: 10
+    };
+  }
+
+  let uCoins = bets[message.author.id].bet;
+
+
+  let coinEmbed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#00FF00")
+  .addDiscription("You bet in arcades.")
+  .addField("💸", uCoins);
+
+  message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+
+}
+
+module.exports.help = {
+  name: "coins"
+}
---
+++ commands/setbet.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+let bets = require("../bets.json");
+
+module.exports.run = async (bot, message, args) => {
+  //!coins
+  await message.delete();
+  let Bet = 9;
+  let betset = args.join(" ");
+  if (betset  <= Bet) return message.replay("You muss give number equal or big than 10");
+  if(!bets[message.author.id]){
+    bets[message.author.id] = {
+      bet: 10
+    };
+  }
+
+  let uCoins = bets[message.author.id].bet;
+
+  uCoins = betset;
+
+  let coinEmbed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#00FF00")
+  .addDiscription("You new bet in arcades.")
+  .addField("💸", uCoins);
+
+  message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+
+    fs.writeFile("./bets.json", JSON.stringify(coins), (err) => {
+        if (err) console.log(err)
+    })
+}
+
+module.exports.help = {
+  name: "coins"
+}
