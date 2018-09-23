a4dbbfda8e8217060a57c1886acf76aba6fc149e
Darek <wojak1011@gmail.com>
2018-09-23 17:44:35 +0200

    324

--- commands/8-ball.js
+++ commands/8-ball.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
     }
     if(args[0] == "Czy" || args[0] == "czy"){
         if(!args[2]) return message.reply("Please ask a full question")
-        let replies = ["Tak.", "Nie.", "Może.", "Nie wiem", "Napewno Tak.", "Napewno Nie.", "Nie Moge Odpowiedzieć na to pytanie."];
+        let replies = ["Tak.", "Nie.", "Może.", "Nie wiem", "Napewno Tak.", "Napewno Nie.", "Nie Moge Odpowiedzieć na to pytanie.", "Chyba tak", "Chyba nie"];
     
         let result = Math.floor((Math.random()* replies.length));
         let question = args.slice(0).join(" ");
@@ -26,7 +26,7 @@ module.exports.run = async (bot, message, args) => {
     if(args[0] == "Kiedy" || args[0] == "kiedy"){
         if(!args[2]) return message.reply("Please ask a full question")
         let number = Math.floor(Math.random() * 29) + 1;
-        let replies = [number + " Stycznia", number + " Lutego", number + " Marca", number + " Kwietnia", number + " Maja", number + " Czerwca", number + " Lipca", number + " Sierpnia", number + " Września", number + " Października", number + " Listopada", number + " Grudnia"];
+        let replies = [number + " Stycznia", number + " Lutego", number + " Marca", number + " Kwietnia", number + " Maja", number + " Czerwca", number + " Lipca", number + " Sierpnia", number + " Września", number + " Października", number + " Listopada", number + " Grudnia", "Jutro", "Za Tydzień", "Za Miesiąc", "Za Rok"];
     
         let result = Math.floor((Math.random()* replies.length));
         let question = args.slice(0).join(" ");
