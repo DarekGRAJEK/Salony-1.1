e30eec11231054d6f9b7853ca277a735a4f4b0c1
Darek <wojak1011@gmail.com>
2018-09-23 17:28:50 +0200

    324

--- commands/8-ball.js
+++ commands/8-ball.js
@@ -6,21 +6,23 @@ module.exports.run = async (bot, message, args) => {
         message.reply("Usage: !8ball <Question>");
         return;
     }
-    if(!args[2]) return message.reply("Please ask a full question")
-    let replies = ["Tak.", "Nie.", "Może.", "Nie wiem", "Napewno Tak.", "Napewno Nie.", "Nie Moge Odpowiedzieć na to pytanie."];
-
-    let result = Math.floor((Math.random()* replies.length));
-    let question = args.slice(0).join(" ");
-    let Color = Math.floor(Math.random() * 999999) + 1;
-    let random = "#" + Color;
-
-    let ballembed = new Discord.RichEmbed()
-    .setAuthor(message.author.tag)
-    .setColor(random)
-    .addField("Question", question)
-    .addField("Answer", replies[result]);
-
-    message.channel.send(ballembed);
+    if(args[0] == "Czy"){
+        if(!args[2]) return message.reply("Please ask a full question")
+        let replies = ["Tak.", "Nie.", "Może.", "Nie wiem", "Napewno Tak.", "Napewno Nie.", "Nie Moge Odpowiedzieć na to pytanie."];
+    
+        let result = Math.floor((Math.random()* replies.length));
+        let question = args.slice(0).join(" ");
+        let Color = Math.floor(Math.random() * 999999) + 1;
+        let random = "#" + Color;
+    
+        let ballembed = new Discord.RichEmbed()
+        .setAuthor(message.author.tag)
+        .setColor(random)
+        .addField("Question", question)
+        .addField("Answer", replies[result]);
+    
+        message.channel.send(ballembed);
+    }
 }
 
 module.exports.help = {
