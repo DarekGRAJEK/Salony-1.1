78eeefd9d9770fb1cbbe0df3dbc3bea25ed0c399
Darek <wojak1011@gmail.com>
2018-09-23 17:48:42 +0200

    33424

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -18,7 +18,7 @@ module.exports.run = async (bot, message, args) => {
     .addField("Bot Name", bot.user.username)
     .addField("Prefix (conventional) is", "**$**")
     .addField("Created by", "DarekGRAJEK")
-    .addField("Version:", "1.5.0");
+    .addField("Version:", "1.5.2");
 
     message.channel.send(botembed);
 
