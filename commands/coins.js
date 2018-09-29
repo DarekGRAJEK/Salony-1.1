32a2bd7d9911645e0fb685a22a3759b3e97b9010
Darek <wojak1011@gmail.com>
2018-09-29 21:49:51 +0200

    12321

--- commands/coins.js
+++ commands/coins.js
@@ -9,23 +9,48 @@ module.exports.run = async (bot, message, args) => {
       coins: 0
     };
   }
+
+  let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if (!pUser) return errors.cantfindUser(channel);
+  if(!coins[pUser.id]){
+    coins[pUser.id] = {
+      coins: 0
+    };
+  }
+  
+
   if(args[0] == "help"){
     message.reply("Usage: !coins");
     return;
   }
+  if (!pUser) {
 
+    let uCoins = coins[message.author.id].coins;
 
-  let uCoins = coins[message.author.id].coins;
+    let Color = Math.floor(Math.random() * 999999) + 1;
+    let random = "#" + Color;
+  
+    let coinEmbed = new Discord.RichEmbed()
+    .setAuthor(message.author.username)
+    .setColor(random)
+    .addField("💸", uCoins);
+  
+    message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+  } else {
+    
+    let pCoins = coins[pUser.id].coins;
 
-  let Color = Math.floor(Math.random() * 999999) + 1;
-  let random = "#" + Color;
+    let Color = Math.floor(Math.random() * 999999) + 1;
+    let random = "#" + Color;
 
-  let coinEmbed = new Discord.RichEmbed()
-  .setAuthor(message.author.username)
-  .setColor(random)
-  .addField("💸", uCoins);
+    let coinEmbed = new Discord.RichEmbed()
+    .setAuthor(pUser.username)
+    .setColor(random)
+    .addField("💸", pCoins);
 
-  message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+    message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+  }
+
 
 }
 
