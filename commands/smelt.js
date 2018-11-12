b8c23b635a0665e8bf0a0189a3672fd6d4701b2e
Darek <wojak1011@gmail.com>
2018-11-12 20:35:41 +0100

    234234

--- commands/smelt.js
+++ commands/smelt.js
@@ -56,13 +56,20 @@ module.exports.run = async (bot, message, args) => {
     let itemlist = collected.first().content;
 
     if(itemlist === "iron") {
-    sur1 = sur1 + parseInt(2);
-    sur2 = sur2 + parseInt(1);
-    get = get + parseInt(1);
-    mno = mno + parseInt(1);
+      multi = parseInt(1);
+      do {
+      sur1 = sur1 + parseInt(2);
+      sur2 = sur2 + parseInt(1);
+      get = get + parseInt(1);
+      mno = mno + parseInt(1);
+      multi = parseInt(multi);
+      sur1 = sur1 * parseInt(multi);
+      sur2 = sur2 * parseInt(multi);
+      mno = mno * parseInt(multi);
+      get = get * parseInt(multi);
       let helpEmbed = new Discord.RichEmbed()
         .setTitle("Smelt Iron")
-        .setDescription("Information to Smelt. (default)")
+        .setDescription("Information to Smelt.")
         .setColor(random)
         .addField("Need Iron Ore", sur1)
         .addField("Neet Coal", sur2)
@@ -70,7 +77,7 @@ module.exports.run = async (bot, message, args) => {
         .addField("Multiplier", mno)
         .addField("If you accept this:", "Please write  ***accept***")
         .addField("If you change Multiplier:", "Please write  ***change***")
-        .addField("If you cancel all progress:", "Please write any letter.");
+        .addField("If you cancel the smelt:", "Please write any letter.");
       message.channel.send(helpEmbed).then(r => r.delete(20000));
       
       message.reply("Im wait to write option.").then(r => r.delete(10000));
@@ -116,96 +123,38 @@ module.exports.run = async (bot, message, args) => {
           .addField("Now Have:", `iron: ${iiron}`);
           message.channel.send(GetSur).then(r => r.delete(15000));
         } else if (option === "change") {
-          message.reply("Please write new Multiplayer. (Write ***Cancel*** to cancel all progress!)").then(r => r.delete(10000));
+          do {
+          message.reply("Please write new Multiplayer. (Write ***Cancel*** to return!)").then(r => r.delete(10000));
           message.channel.awaitMessages(filter, {max: 1, time: 20000}).then(collected => {
           
           if(collected.first().content === "cancel") {
-            return message.reply("Cancled");
+            message.reply("Returned!").then(r => r.delete(4000));
+            break;
           }
       
           let multi = collected.first().content;
           
-          if (multi == null) return message.reply("This is not Number! Smelt Progress has canceled!");
-          multi = parseInt(multi);
-          sur1 = sur1 * parseInt(multi);
-          sur2 = sur2 * parseInt(multi);
-          mno = mno * parseInt(multi);
-          get = get * parseInt(multi);
-          if (multi >= 1) { 
-          let helpEmbed = new Discord.RichEmbed()
-          .setTitle("Smelt Iron")
-          .setDescription("Information to Smelt. (changed)")
-          .setColor(random)
-          .addField("Need Iron Ore", sur1)
-          .addField("Neet Coal", sur2)
-          .addField("Get Iron", get)
-          .addField("Multiplier", mno)
-          .addField("If you accept this:", "Please write  ***accept***")
-          .addField("If you cancel all progress:", "Please write any letter.");
-          message.channel.send(helpEmbed).then(r => r.delete(15000));
-          message.reply("I'm waiting for write a option.").then(r => r.delete(10000));
-          message.channel.awaitMessages(filter, {max: 1, time: 20000}).then(collected => {
-          let option = collected.first().content;
-
-            if (option === "accept"){
-              let iron = invt[message.author.id].id1;
-              let coal = invt[message.author.id].id7;
-              let iiron = invt[message.author.id].id101;
-              let Need = new Discord.RichEmbed()
-              .setTitle("Progress Canceled!")
-              .setColor(random)
-              .addField("You Need:", `iron ore: ${sur1}`)
-              .addField("You Have:", `iron ore: ${iron}`);
-              if(iron < sur1) return message.channel.send(Need).then(r => r.delete(5000));
-              let Need2 = new Discord.RichEmbed()
-              .setTitle("Progress Canceled!")
-              .setColor(random)
-              .addField("You Need:", `coal: ${sur2}`)
-              .addField("You Have:", `coal: ${coal}`);
-              if(coal < sur1) return message.channel.send(Need2).then(r => r.delete(5000));
-    
-              invt[message.author.id].id101 = iiron + parseInt(get);
-              invt[message.author.id].id1 = iron - parseInt(sur1);
-              invt[message.author.id].id7 = coal - parseInt(sur2);
-    
-              fs.writeFile("./inventory.json", JSON.stringify(invt), (err) => {
-                if(err) cosole.log(err)
-              });
-              iron = invt[message.author.id].id1;
-              iiron = invt[message.author.id].id101;
-              coal = invt[message.author.id].id7;
-              let GetSur = new Discord.RichEmbed()
-              .setTitle("You completed smelt progress!")
-              .setColor(random)
-              .setDescription("Hurry!!! You Happy DAY!")
-              .addField("Now Have:", `iron ore: ${iron}`)
-              .addField("Now Have:", `coal: ${coal}`)
-              .addField("You Get:", `iron: ${get}`)
-              .addField("Now Have:", `iron: ${iiron}`);
-              message.channel.send(GetSur).then(r => r.delete(15000));    
-            } else {
-              return message.reply("Canceled!").then(r => r.delete(5000));
-            }
-
-          }).catch(err => {
-            message.reply("Canceled!").then(r => r.delete(5000));
-          });
-          } else {
-            return message.reply("The number must be positive");
+          if (multi == null) message.reply("This is not Number!");
+          if (multi < 1) message.reply("The number muss be over 0.")
+          if (multi >= 1) {
+            break;
           }
-
+        
           }).catch(err => {
-            message.reply("Canceled!").then(r => r.delete(5000));
+            return message.reply("Time Out!").then(r => r.delete(5000));
           });
+          } while (true)
+
         } else {
-          message.reply("Canceled!").then(r => r.delete(5000));
+          return message.reply("Canceled!").then(r => r.delete(5000));
         }
 
       }).catch(err => {
-      message.reply("Canceled!").then(r => r.delete(5000));
+       return message.reply("Time Out!").then(r => r.delete(5000));
       });
+     } while (true)
     }
-   
+  
   }).catch(err => {
     message.reply("Canceled!").then(r => r.delete(5000));
   });
